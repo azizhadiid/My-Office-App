d968a96da1d7e9dccfcd0a4ee30dd56cbed5e7ab
azizhadiid <azizalhadiid88@gmail.com>
2025-06-20 13:45:10 +0700

    Props Data Office Card is to be Continue

--- src/features/cities/sections/CitiSections.tsx
+++ src/features/cities/sections/CitiSections.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import CityCard from "../components/CityCard";
+import { cities } from "../data/cities.mock";
+
 const CitiSections = () => {
     return (
         <section id="Cities" className="flex flex-col gap-[30px] mt-[100px]">
@@ -8,34 +14,25 @@ const CitiSections = () => {
                 </h2>
                 <a
                     href="#"
-                    className="rounded-full rounded-full py-3 px-5 bg-white font-bold"
+                    className="rounded-full py-3 px-5 bg-white font-bold"
                 >
                     Explore All City
                 </a>
             </div>
-            <div className="swiper w-full">
+            <Swiper
+                spaceBetween={30}
+                slidesOffsetAfter={30}
+                slidesOffsetBefore={30}
+                slidesPerView="auto"
+                className="swiper w-full">
                 <div className="swiper-wrapper">
-
-                    <div className="swiper-slide !w-fit first-of-type:pl-[calc((100%-1130px-60px)/2)] last-of-type:pr-[calc((100%-1130px-60px)/2)]">
-                        <a href="city-details.html" className="card">
-                            <div className="relative flex shrink-0 w-[230px] h-[300px] rounded-[20px] overflow-hidden">
-                                <div className="relative flex flex-col justify-end w-full h-full p-5 gap-[2px] bg-[linear-gradient(180deg,_rgba(0,0,0,0)_49.87%,_rgba(0,0,0,0.8)_100%)] z-10">
-                                    <h3 className="font-bold text-xl leading-[30px] text-white">
-                                        Jakarta Pusat
-                                    </h3>
-                                    <p className="text-white">189 Offices</p>
-                                </div>
-                                <img
-                                    src="/assets/images/thumbnails/thumbnails-2.png"
-                                    className="absolute w-full h-full object-cover"
-                                    alt="thumbnails"
-                                />
-                            </div>
-                        </a>
-                    </div>
-
+                    {cities.map((city) => (
+                        <SwiperSlide key={city.id} className="swiper-slide !w-fit first-of-type:pl-[calc((100%-1130px-60px)/2)] last-of-type:pr-[calc((100%-1130px-60px)/2)]">
+                            <CityCard city={city} />
+                        </SwiperSlide>
+                    ))}
                 </div>
-            </div>
+            </Swiper>
         </section>
     );
 };
